aed6a0c37dd8bd6e27e006c215371dc57f4b7e51
Niko Wilbert <mail@nikowilbert.de>
2022-08-10 15:48:52 +0200

    switched to patchlib, filter hidden dirs & files

--- src/pyarch/main.py
+++ src/pyarch/main.py
@@ -1,8 +1,10 @@
+from pathlib import Path
+
 from pyarch.parser import build_import_model
 
 
 def main():
-    base_path = '/Users/niko/projects/bobbytime/bobbytime'
+    base_path = Path('/Users/niko/projects/bobbytime/bobbytime')
     root_node = build_import_model(base_path)
     pass
 
--- src/pyarch/parser.py
+++ src/pyarch/parser.py
@@ -1,16 +1,15 @@
 import ast
-import os
-import pathlib
+from pathlib import Path
 from typing import Sequence, Generator
 
 from .model import Node, Import
 
 
-def build_import_model(base_path: str) -> Node:
+def build_import_model(base_path: Path) -> Node:
     root_node = Node(name='')
     for module_path in _walk_modules(base_path):
         with open(module_path) as module_file:
-            module_ast = ast.parse(module_file.read(), module_path)
+            module_ast = ast.parse(module_file.read(), str(module_path))
         if not module_ast.body:
             continue
         node = root_node.get_or_add(_get_node_path(base_path, module_path))
@@ -20,8 +19,8 @@ def build_import_model(base_path: str) -> Node:
     return root_node
 
 
-def _get_node_path(base_path: str, module_path: str) -> Sequence[str]:
-    node_path = list(pathlib.PurePath(os.path.relpath(module_path, base_path)).parts)
+def _get_node_path(base_path: Path, module_path: Path) -> Sequence[str]:
+    node_path = list(module_path.relative_to(base_path).parts)
     if node_path[-1] == '__init__.py':
         node_path.pop()
     else:
@@ -52,8 +51,7 @@ def _collect_imports(module_ast: ast.Module) -> Sequence[Import]:
     return imports
 
 
-def _walk_modules(base_path: str) -> Generator[str, None, None]:
-    for root, _, file_names in os.walk(base_path):
-        for file_name in file_names:
-            if file_name.endswith('.py'):
-                yield os.path.join(root, file_name)
+def _walk_modules(base_path: Path) -> Generator[Path, None, None]:
+    for path in base_path.glob('**/*.py'):
+        if not any(part.startswith('.') for part in path.parts):
+            yield path
